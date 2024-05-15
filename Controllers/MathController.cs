1f557c801cd79a7a570ae0b9584f6e50c5f40b58
PC09 <PC09@INFO6-5472>
2024-05-15 08:48:17 +0200

    feat: added the method " celsius_to_farenheit" to convert the celsius temperature into farenheit temperature

---
+++ Controllers/MathController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AS2324_5G_INF_AngeliniAlex_WebAPIrec.Controllers
+{
+    public class MathController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+            
+        }
+        [HttpGet("celsius_to_farenheit")]
+        public JsonResult celsius_to_farenheit(int celsius) 
+        {
+            return new JsonResult(new 
+            {
+                message = "Temperatura in farenheit:",
+                result = (celsius * 9/5) + 32,
+            });
+        }
+    }
+}
