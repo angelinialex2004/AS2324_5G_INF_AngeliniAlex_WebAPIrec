2d1e5e8eedaef3959a464959896f98679f3bcc56
PC09 <PC09@INFO6-5472>
2024-05-15 09:02:58 +0200

    fixed the names of the methods in PascalCase

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -11,25 +11,26 @@ namespace AS2324_5G_INF_AngeliniAlex_WebAPIrec.Controllers
 
 
         }
-        [HttpGet("celsius_to_farenheit")]
-        public JsonResult celsius_to_farenheit(int celsius)
+
+        [HttpGet("CelsiusToFarenheit")]
+        public JsonResult CelsiusToFarenheit(int celsius)
         {
             return new JsonResult(new
             {
                 message = "Temperatura in farenheit:",
                 result = (celsius * 9 / 5) + 32,
-                status = "OK"
+                status = "OK",
             });
         }
 
-        [HttpGet("volume_cylinder")]
-        public JsonResult volume_cylinder(float radius, float height)
+        [HttpGet("VolumeCylinder")]
+        public JsonResult VolumeCylinder(float radius, float height)
         {
             return new JsonResult(new
             {
                 message = "Il volume del cilindro è:",
                 result = Math.PI * (radius * radius) * height,
-                status = "OK"
+                status = "OK",
             });
         }
     }
